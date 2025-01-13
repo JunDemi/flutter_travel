679deff3825b9bfb1506c0ef5e51a1f7debdb3d0
JunDemi <jungwook3176@gmail.com>
2025-01-13 18:58:17 +0900

    메인 UI

---
+++ lib/layouts/cardContainer.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_travel/widgets/card.dart';
+
+// ignore: use_key_in_widget_constructors
+class CardContainer extends StatelessWidget {
+  final List<Map<String, dynamic>> currencyData = [
+    {
+      'name': 'Euro',
+      'code': 'EUR',
+      'amount': '6 428',
+      'icon': Icons.euro_rounded,
+    },
+    {
+      'name': 'Dollar',
+      'code': 'USD',
+      'amount': '55 622',
+      'icon': Icons.attach_money_rounded,
+    },
+    {
+      'name': 'Bitcoin',
+      'code': 'BTC',
+      'amount': '7 928',
+      'icon': Icons.currency_bitcoin_rounded,
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        for (int i = 0; i < currencyData.length; i++)
+          CurrencyCard(
+            name: currencyData[i]['name'],
+            code: currencyData[i]['code'],
+            amount: currencyData[i]['amount'],
+            icon: currencyData[i]['icon'],
+            isInverted: i % 2 != 0 || false,
+            y: i * -20,
+          )
+      ],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_travel/layouts/cardContainer.dart';
+import 'package:flutter_travel/widgets/button.dart';
+import 'package:flutter_travel/widgets/card.dart';
 
 void main() {
   runApp(App());
@@ -10,41 +13,134 @@ class App extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Color(0xFF181818),
-        body: Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: 40,
-          ),
-          child: Column(
-            children: [
-              SizedBox(
-                height: 80,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Text(
-                        'Hey, Selena',
-                        style: TextStyle(
-                          fontSize: 28,
-                          fontWeight: FontWeight.w600,
-                          color: Colors.white,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: 20,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 80,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          'Hey, Selena',
+                          style: TextStyle(
+                            fontSize: 28,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.white,
+                          ),
                         ),
-                      ),
-                      Text(
-                        'Welcome back',
-                        style: TextStyle(
-                          color: Colors.white.withValues(alpha: 0.8),
-                          fontSize: 18,
+                        Text(
+                          'Welcome back',
+                          style: TextStyle(
+                            color: Colors.white.withValues(alpha: 0.8),
+                            fontSize: 18,
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 70,
+                ),
+                Text(
+                  'Total Balance',
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.white.withValues(alpha: 0.8),
                   ),
-                ],
-              ),
-            ],
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  '\$5 194 482',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 34,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Button(
+                        text: 'Transfer',
+                        bgColor: Color(0xFFF2B33A),
+                        textColor: Colors.black),
+                    Button(
+                        text: 'Request',
+                        bgColor: Color(0xFF1F2123),
+                        textColor: Colors.white),
+                  ],
+                ),
+                SizedBox(
+                  height: 70,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Wallets',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      'View All',
+                      style: TextStyle(
+                        color: Colors.white.withValues(alpha: 0.8),
+                        fontSize: 12,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                CardContainer(),
+                // CurrencyCard(
+                //   name: 'Euro',
+                //   code: 'EUR',
+                //   amount: '6 428',
+                //   icon: Icons.euro_rounded,
+                // ),
+                // Transform.translate(
+                //   offset: Offset(0, -25),
+                //   child: CurrencyCard(
+                //     name: 'Dollar',
+                //     code: 'USD',
+                //     amount: '55 622',
+                //     icon: Icons.attach_money_rounded,
+                //     isInverted: true,
+                //   ),
+                // ),
+                // Transform.translate(
+                //   offset: Offset(0, -50),
+                //   child: CurrencyCard(
+                //     name: 'Bitcoin',
+                //     code: 'BTC',
+                //     amount: '7 928',
+                //     icon: Icons.currency_bitcoin_rounded,
+                //   ),
+                // ),
+              ],
+            ),
           ),
         ),
       ),
